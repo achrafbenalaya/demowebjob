3edc8460e5dc45ba52cdf881d68bbadc0ad6077b
Ben_A <ben_alaya_achraf@outlook.com>
2021-08-19 10:26:02 +0200

    Add project files.

---
+++ demowebjob/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+
+namespace demowebjob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((loggingBuilder) =>
+            {
+                loggingBuilder.AddConsole();
+            });
+
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+
+
+        public class Functions
+        {
+            public static void ProcessQueueMessage([QueueTrigger("my-queue")] string message, ILogger logger)
+            {
+                logger.LogInformation("START of Execution of your Web Job");
+                logger.LogInformation(message);
+                logger.LogInformation("END of Execution of your Web Job");
+            }
+        }
+
+
+    }
+}
